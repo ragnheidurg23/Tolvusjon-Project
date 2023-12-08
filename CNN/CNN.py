f0cbcf97ce73c65921fb30ad0a7d392928e7902b
ragnheidurgna <ragnheidurg23@ru.is>
2023-12-08 14:37:59 +0000

    halfway

--- CNN/CNN.py
+++ CNN/CNN.py
@@ -1,27 +1,82 @@
 import tensorflow as tf
-from tensorflow.keras import layers, models
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+import matplotlib.pyplot as plt
+import os
 
-# Define a simple CNN model
-model = models.Sequential()
+# Define your model as shown in the previous example
 
-# Convolutional layers
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(image_height, image_width, 1)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(128, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
+# Path to your dataset
+data_dir = 'path/to/your/dataset'
 
-# Flatten layer to transition from convolutional layers to fully connected layers
-model.add(layers.Flatten())
+# Get the list of all image files in the dataset
+all_image_files = [os.path.join(data_dir, file) for file in os.listdir(data_dir) if file.endswith('.jpg')]
 
-# Fully connected layers
-model.add(layers.Dense(128, activation='relu'))
-model.add(layers.Dropout(0.5))  # Dropout for regularization
-model.add(layers.Dense(1, activation='sigmoid'))  # Binary classification, use 'softmax' for multiclass tasks
+# Create labels based on the assumption that positive examples are in a subdirectory named 'positive'
+labels = [1 if 'positive' in file else 0 for file in all_image_files]
+
+# Split the data into train, validation, and test sets
+train_files, test_files, train_labels, test_labels = train_test_split(all_image_files, labels, test_size=0.2, random_state=42)
+val_files, test_files, val_labels, test_labels = train_test_split(test_files, test_labels, test_size=0.5, random_state=42)
+
+# Create data generators for training, validation, and test
+train_datagen = ImageDataGenerator(rescale=1./255)
+train_generator = train_datagen.flow_from_directory(
+    data_dir,
+    target_size=(image_height, image_width),
+    batch_size=batch_size,
+    class_mode='binary',  # Use 'categorical' for multiclass tasks
+    subset='training'
+)
+
+val_datagen = ImageDataGenerator(rescale=1./255)
+val_generator = val_datagen.flow_from_directory(
+    data_dir,
+    target_size=(image_height, image_width),
+    batch_size=batch_size,
+    class_mode='binary',  # Use 'categorical' for multiclass tasks
+    subset='validation'
+)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+test_generator = test_datagen.flow_from_directory(
+    data_dir,
+    target_size=(image_height, image_width),
+    batch_size=batch_size,
+    class_mode='binary'  # Use 'categorical' for multiclass tasks
+)
 
 # Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-# Display the model summary
-model.summary()
+# Define callbacks for training
+callbacks = [
+    tf.keras.callbacks.ModelCheckpoint(filepath='best_model.h5', save_best_only=True),
+    tf.keras.callbacks.EarlyStopping(patience=5, restore_best_weights=True),
+    tf.keras.callbacks.TensorBoard(log_dir='./logs')
+]
+
+# Train the model
+history = model.fit(
+    train_generator,
+    epochs=epochs,
+    validation_data=val_generator,
+    callbacks=callbacks
+)
+
+# Plot training history
+plt.plot(history.history['accuracy'], label='accuracy')
+plt.plot(history.history['val_accuracy'], label='val_accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.ylim([0, 1])
+plt.legend(loc='lower right')
+plt.show()
+
+# Evaluate the model on the test dataset
+predictions = model.predict(test_generator)
+y_pred = (predictions > 0.5).astype(int)
+
+# Print classification report
+print(classification_report(test_generator.classes, y_pred))
